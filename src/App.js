25ee14954488ba8af8ecd1de4ab80d1f7d096ef6
Thatianne Carvalho <thatianne.cristina@hotmail.com>
2020-01-19 12:08:39 -0300

    changed component function to class

--- src/App.js
+++ src/App.js
@@ -1,95 +1,134 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-function App() {
-  return (
-    <div id="app">
-      <aside>
-        <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="username">Usuário do Github</label>
-            <input name="username" id="username" required />
-          </div>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      githubUsername: '',
+      techs: ''
+    }
+  }
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords
+        this.setState({
+          latitude,
+          longitude
+        })
+      },
+      err => console.log(err),
+      {
+        timeout: 30000
+      })
+  }
 
-          <div className="input-group">
+  render() {
+    return (
+      <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <form>
             <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <label htmlFor="username">Usuário do Github</label>
+              <input name="username" id="username" required />
             </div>
 
             <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required />
             </div>
-          </div>
 
-          <button type="submit">Salvar</button>
-
-        </form>
-      </aside>
-      <main>
-        <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
-              <div className="user-info">
-                <strong>Thatianne Carvalho</strong>
-                <span>Javascript, Vue, PHP</span>
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input
+                  type="number"
+                  name="latitude"
+                  id="latitude"
+                  value={this.state.latitude}
+                  onChange={e => this.setState({ latitude: e.target.value })}
+                  required
+                />
               </div>
-            </header>
-            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
-            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
-          </li>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
-              <div className="user-info">
-                <strong>Thatianne Carvalho</strong>
-                <span>Javascript, Vue, PHP</span>
+              <div className="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input
+                  type="number"
+                  name="longitude"
+                  id="longitude"
+                  value={this.state.longitude}
+                  onChange={e => this.setState({ longitude: e.target.value })}
+                  required
+                />
               </div>
-            </header>
-            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
-            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
-          </li>
+            </div>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
-              <div className="user-info">
-                <strong>Thatianne Carvalho</strong>
-                <span>Javascript, Vue, PHP</span>
-              </div>
-            </header>
-            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
-            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
-          </li>
+            <button type="submit">Salvar</button>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
-              <div className="user-info">
-                <strong>Thatianne Carvalho</strong>
-                <span>Javascript, Vue, PHP</span>
-              </div>
-            </header>
-            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
-            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
-          </li>
-        </ul>
-      </main>
-    </div>
-  );
+          </form>
+        </aside>
+        <main>
+          <ul>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+                <div className="user-info">
+                  <strong>Thatianne Carvalho</strong>
+                  <span>Javascript, Vue, PHP</span>
+                </div>
+              </header>
+              <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+              <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+            </li>
+
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+                <div className="user-info">
+                  <strong>Thatianne Carvalho</strong>
+                  <span>Javascript, Vue, PHP</span>
+                </div>
+              </header>
+              <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+              <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+            </li>
+
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+                <div className="user-info">
+                  <strong>Thatianne Carvalho</strong>
+                  <span>Javascript, Vue, PHP</span>
+                </div>
+              </header>
+              <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+              <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+            </li>
+
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+                <div className="user-info">
+                  <strong>Thatianne Carvalho</strong>
+                  <span>Javascript, Vue, PHP</span>
+                </div>
+              </header>
+              <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+              <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+            </li>
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App
