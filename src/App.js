27a8b0ab9fc130e6f2369c06a22be0fed9657f7d
Thatianne Carvalho <thatianne.cristina@hotmail.com>
2020-01-19 11:19:32 -0300

    created page style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="username">Usuário do Github</label>
+            <input name="username" id="username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+              <div className="user-info">
+                <strong>Thatianne Carvalho</strong>
+                <span>Javascript, Vue, PHP</span>
+              </div>
+            </header>
+            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+              <div className="user-info">
+                <strong>Thatianne Carvalho</strong>
+                <span>Javascript, Vue, PHP</span>
+              </div>
+            </header>
+            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+              <div className="user-info">
+                <strong>Thatianne Carvalho</strong>
+                <span>Javascript, Vue, PHP</span>
+              </div>
+            </header>
+            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/14905849?v=4" alt="Thatianne Carvalho" />
+              <div className="user-info">
+                <strong>Thatianne Carvalho</strong>
+                <span>Javascript, Vue, PHP</span>
+              </div>
+            </header>
+            <p>Desenvolvedora web e estudante de Engenharia de computação </p>
+            <a href="https://github.com/Thatianne">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
